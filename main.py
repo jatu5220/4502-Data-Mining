1cde8db81d8f1fc26db80d79f22543ac37c65a85
Henry Tuten <jatu5220@colorado.edu>
2023-07-24 23:43:58 -0600

    add misc mining techniques

--- main.py
+++ main.py
@@ -2,30 +2,23 @@ import csv
 import pandas as pd
 import re
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.cluster import KMeans
 
 #-------------------DATA GATHERING----------------------------
 
-# open first csv file
-with open('datasets/finace_student_loan_debt/balance_by_age.csv', 'r') as file1:
-    balance_by_age = csv.reader(file1)
+# Load the data
+balance_by_age = pd.read_csv('datasets/finace_student_loan_debt/balance_by_age.csv')
+debt_distribution = pd.read_csv('datasets/finace_student_loan_debt/debt_amt_distribution2014.csv')
+home_secured_debt = pd.read_csv('datasets/finace_student_loan_debt/home_secured_debt_age30.csv')
+non_mort_balance = pd.read_csv('datasets/finace_student_loan_debt/non_mort_balance.csv')
 
-
-# open second csv file
-with open('datasets/finace_student_loan_debt/debt_amt_distribution2014.csv', 'r') as file2:
-    debt_distribution = csv.reader(file2)
-
-
-# open third csv
-with open('datasets/finace_student_loan_debt/home_secured_debt_age30.csv', 'r') as file3:
-    home_secured_debt = csv.reader(file3)
-
-
-# open fourth csv
-with open('datasets/finace_student_loan_debt/non_mort_balance.csv', 'r') as file4:
-    non_mort_balance = csv.reader(file4)
 #--------------------------------------------------------------
 
 
@@ -63,7 +56,6 @@ def remove_outliers(df, column):
 
 
 #------------------DISTRIBUTION ANALYSIS-----------------------------------
-
 
 debt_distribution['NumberOfBorrowers'] = debt_distribution['NumberOfBorrowers'].str.replace(',', '').astype(int)
 debt_distribution.plot(x='Balance2014', y='NumberOfBorrowers', kind='bar', legend=False)
@@ -113,24 +105,83 @@ print("Accuracy:", accuracy_score(y_test, y_pred))
 #---------------------------------------------------------------
 
 
-#--------------------REGRESSION---------------------------------
+#--------------------TIME SERIES FOR NON MORT---------------------------------
 
+# Set 'Time' as index
+non_mort_balance['Time'] = pd.to_datetime(non_mort_balance['Time'])
+non_mort_balance.set_index('Time', inplace=True)
 
-
-
-
-
+# Plotting the time series data
+non_mort_balance.plot()
+plt.show()
 
 #---------------------------------------------------------------
 
 #---------------------MACHINE LEARNING PREDICTION---------------
 
 
+# Prepare the features (X) and the target (y)
+X = non_mort_balance.index.values.reshape(-1,1)
+y = non_mort_balance['Auto Loan']
+
+# Split the data into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Train the model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Predict on the test data
+y_pred = model.predict(X_test)
+
+# Print the RMSE
+print("RMSE:", np.sqrt(mean_squared_error(y_test, y_pred)))
 
 
+#---------------------------------------------------------------
+
+#-------------------CLUSTER ANALYSIS-----------------------------
+
+# Cluster periods based on loan values
+kmeans = KMeans(n_clusters=3, random_state=0)
+clusters = kmeans.fit_predict(non_mort_balance[['HELOC', 'Auto Loan', 'Credit Card', 'Student Loan', 'Other']])
+non_mort_balance['Cluster'] = clusters
+
+# Check the result
+print(non_mort_balance.head())
 
 
+#---------------------------------------------------------------
+
+#---------------TIME SERIES BALANCE BY AGE------------------------------
+
+# Set 'Year' as index
+balance_by_age['Year'] = pd.to_datetime(balance_by_age['Year'], format='%Y')
+balance_by_age.set_index('Year', inplace=True)
+
+# Plotting the time series data
+balance_by_age.plot()
+plt.show()
+
+#---------------------------------------------------------------
 
 
+#---------------FORECASTING BY AGE------------------------------
 
+# Prepare the features (X) and the target (y)
+X = balance_by_age.index.year.values.reshape(-1, 1)
+y = balance_by_age['under30']
+
+# Split the data into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Train the model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Predict on the test data
+y_pred = model.predict(X_test)
+
+# Print the RMSE
+print("RMSE:", np.sqrt(mean_squared_error(y_test, y_pred)))
 #---------------------------------------------------------------
